b5bfe46b5fa3e28d14c591e6047eb6b046139bfe
Abdi kumela <abdikumela@036gmail.com>
2024-02-23 18:09:38 -0800

    added api key

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// const inputValue = document.querySelector(".inputPlace");
+// const searchBtn = document.querySelector(".btn");
+// const weatherImg = document.querySelector(".weather_image");
+// // const cityName = document.querySelector(".city_name");
+const cityName = "addis ababa";
+
+const API_key = "daeea96cab56a856c044a0803a6c59fa";
+const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_key}&units=metric`;
+
+// creating async await function
+
+async function weatherCheck() {
+  const res = await fetch(URL);
+  const data = await res.json();
+  console.log(data);
+}
+weatherCheck();
