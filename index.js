77a2286a9be78653d6239180639c8e4b72492ea2
Abdi kumela <abdikumela@036gmail.com>
2024-02-23 18:53:46 -0800

    repalace all the values from the API to UI

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 // const inputValue = document.querySelector(".inputPlace");
 // const searchBtn = document.querySelector(".btn");
-// const weatherImg = document.querySelector(".weather_image");
-// // const cityName = document.querySelector(".city_name");
-const cityName = "addis ababa";
+const Temprature = document.querySelector(".temprature");
+const feelsLike = document.querySelector(".feelsLike");
+const windSpeed = document.querySelector(".speed");
+const Humidity = document.querySelector(".humidity");
+const cityName = document.querySelector(".city_name");
+const citName = "new york";
 
 const API_key = "daeea96cab56a856c044a0803a6c59fa";
-const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_key}&units=metric`;
+const URL = `https://api.openweathermap.org/data/2.5/weather?q=${citName}&appid=${API_key}&units=metric`;
 
 // creating async await function
 
 async function weatherCheck() {
   const res = await fetch(URL);
   const data = await res.json();
-  console.log(data);
+  cityName.textContent = data.name;
+  Temprature.textContent = data.main.temp + " °C";
+  windSpeed.textContent = data.wind.speed + " km/hr";
+  feelsLike.textContent = data.main.feels_like;
+  Humidity.textContent = data.main.humidity + "%";
 }
 weatherCheck();
